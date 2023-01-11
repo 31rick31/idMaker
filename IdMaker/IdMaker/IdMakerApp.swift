1600fff0b432d8b0da60cc8c3b05bdf45a235af9
Ricky Memije <rickymemije@gmail.com>
2023-01-10 23:49:36 -0700

    starter project files

---
+++ IdMaker/IdMaker/IdMakerApp.swift
@@ -0,0 +1,17 @@
+//
+//  IdMakerApp.swift
+//  IdMaker
+//
+//  Created by Ricky Memije on 1/10/23.
+//
+
+import SwiftUI
+
+@main
+struct IdMakerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
